518b432fa86edf89f8a9f7a947e3cc9d4599c836
zhaozichuan <zhaozichuan@sina.com>
2018-01-19 10:59:11 +0800

    new import

---
+++ newsIn.py
@@ -0,0 +1,31 @@
+import tushare as ts
+import time  
+from sqlalchemy import create_engine
+engine = create_engine('mysql://root:Zzc7382788@rm-uf65wbvomnp2mufa6o.mysql.rds.aliyuncs.com:3306/playebean?charset=utf8')   
+  
+def getNews():
+    df = ts.get_latest_news()
+    df1 = df[['title','url','classify','time']]
+    # print df1
+    #insert()
+    #存入数据库
+    #df1.to_sql('news',engine, if_exists='append',index_label='url')
+    #df1.to_sql('news',engine, if_exists='append',index_label='title,url,classify,time',index=False)
+   
+    print(engine.execute('delete from news'))
+    df1.to_sql('news',engine,if_exists='append', index_label='title,url,classify,time',index=False)
+    
+    print('---ok---') 
+#for i in range(5):  
+while 1 : 
+#   print time.time()  
+#   df= ts.get_latest_news(top=5,show_content=True) #显示最新5条新闻，并打印出新闻内容
+    print time.time()  
+    getNews() 
+    time.sleep(60)  
+   
+   
+   
+   
+   
+   
